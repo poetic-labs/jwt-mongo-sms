282cd9502cfedb76892df4c375cb00b7f4e4e123
Evan Corl <evancorl@yahoo.com>
2017-10-09 20:59:29 -0500

    feat(index): export getCallHandler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import 'babel-polyfill';
 import JwtMongoSms from './JwtMongoSms';
+import getCallHandler from './getCallHandler';
+
+export { JwtMongoSms };
+export { getCallHandler };
 
 export default JwtMongoSms;
